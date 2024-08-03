f67d756584bcf8eed61cf3b02f47e19c26c8aa37
VINXIS <oykxf2@gmail.com>
2024-08-03 09:43:58 -0600

    change frontend to sveltekit

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,3 @@
+Yo
+
+<slot></slot>
---
+++ frontend/src/routes/thread/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    const post = {
+        title: `Thread #${$page.params.id}`,
+        content: `This is the content of thread #${$page.params.id}`
+    }
+</script>
+
+<div class="thread">
+    {post.title}
+    {post.content}
+</div>
