51c13a948e5bd423142627b88ba20e54cf6d39d1
VINXIS <oykxf2@gmail.com>
2024-08-20 10:26:51 -0600

    boards table in db

--- backend/db.ts
+++ backend/db.ts
@@ -1,29 +1,40 @@
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database("board.db", (err) => {
-	if (err) {
+	if (err)
 		console.error(`Error loading database: ${err.message}`);
-	} else {
+	else {
 		console.log("Connected to the database.");
 		// TODO: Maybe fix this from the database side instead of here
 		db.run("PRAGMA foreign_keys = ON", (err) => {
-			if (err) {
+			if (err)
 				console.error(`Failed to enable foreign keys!: ${err.message}`);
-			} else {
+			else
 				console.log("Foreign keys now enabled!");
-			}
 		});
 	}
 });
 
 db.serialize(() => {
+	// query for `boards` table
+	db.run(`
+		CREATE TABLE IF NOT EXISTS boards (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)
+	`);
+
 	// query for `threads` table
 	db.run(`
         CREATE TABLE IF NOT EXISTS threads (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+		board_id INTEGER NOT NULL,       
         title TEXT NOT NULL,
         content VARCHAR(500) NOT NULL, 
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP        
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (board_id) REFERENCES boards(id)
         )
     `);
 
